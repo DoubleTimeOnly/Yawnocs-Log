6e185324dd3a08e3e1478655d9c335a87ff23b7c
Alexander Marzot <43010352+amarzot@users.noreply.github.com>
2024-11-03 18:34:52 -0500

    Rename brute_force_search to pattern_search

--- src/yawnoc/solvers/solver_factory.py
+++ src/yawnoc/solvers/solver_factory.py
@@ -1,12 +1,16 @@
 from enum import Enum
 
 from yawnoc.solvers import brute_force_search
+from yawnoc.solvers import pattern_search
 
 
 class SolverType(Enum):
     BRUTE = 1
+    PATTERN = 2
 
 def get_backward_fn(solver_type: SolverType):
     if solver_type is SolverType.BRUTE:
         return brute_force_search.backward
+    if solver_type is SolverType.PATTERN:
+        return pattern_search.backward
     return None
